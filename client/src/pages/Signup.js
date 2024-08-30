95ee5a5e0d757c01855e81fa1fca748ddd0f941b
Najaf <118128991+Najaf1705@users.noreply.github.com>
2024-08-30 12:01:41 +0530

    Update Signup.js

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -34,7 +34,7 @@ function Signup() {
             return handleWarning("All fields are required");
         }
         try {
-            const url="http://localhost:8080/auth/signup";
+            const url="https://auth-api-steel.vercel.app/auth/signup";
             const response=await fetch(url,{
                 method: "POST",
                 headers: {
